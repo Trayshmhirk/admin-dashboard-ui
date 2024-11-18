a3190d0b7f3ed0243ddc44b8e4bb14a0017918e7
Frank Micheal <harlex.mikkey@gmail.com>
2024-11-18 08:23:16 +0100

    initialize shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
    return (
-      <div className="">
+      <div className="p-3 flex flex-col gap-3 items-start">
          <h1 className="text-3xl font-bold">HOME</h1>
       </div>
    );
