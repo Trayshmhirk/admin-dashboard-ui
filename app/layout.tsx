c7ad5429298f8f1c74bff4b8d2d4c034bd68db1f
Frank Micheal <harlex.mikkey@gmail.com>
2024-11-18 10:00:15 +0100

    set up sidebar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
 
 export const metadata: Metadata = {
    title: "Admin Dashboard UI",
@@ -15,11 +16,13 @@ export default function RootLayout({
    return (
       <html lang="en">
          <body className="min-h-screen flex">
-            <aside>Sidebar</aside>
+            <Sidebar />
 
             <div className="w-full flex flex-col">
                <Header />
-               <main className="">{children}</main>
+               <main className="h-full w-full md:max-w-[1140px]">
+                  {children}
+               </main>
             </div>
          </body>
       </html>
---
+++ components/Sidebar.tsx
@@ -0,0 +1,74 @@
+import {
+   Command,
+   CommandEmpty,
+   CommandGroup,
+   CommandInput,
+   CommandItem,
+   CommandList,
+   CommandSeparator,
+   CommandShortcut,
+} from "@/components/ui/command";
+import {
+   LayoutDashboard,
+   Newspaper,
+   Folders,
+   CreditCard,
+   Settings,
+   User,
+} from "lucide-react";
+import Link from "next/link";
+
+const Sidebar = () => {
+   return (
+      <aside className="hidden md:flex w-[300px]">
+         <Command className="bg-secondary rounded-none">
+            <CommandInput placeholder="Type a command or search..." />
+            <CommandList>
+               <CommandEmpty>No results found.</CommandEmpty>
+               <CommandGroup heading="Suggestions">
+                  <CommandItem>
+                     <Link href="/" className="flex items-center">
+                        <LayoutDashboard className="mr-2 h-4 w-4" />
+                        Dashboard
+                     </Link>
+                  </CommandItem>
+                  <CommandItem>
+                     <Link href="/post" className="flex items-center">
+                        <Newspaper className="mr-2 h-4 w-4" />
+                        Posts
+                     </Link>
+                  </CommandItem>
+                  <CommandItem>
+                     <Link href="/" className="flex items-center">
+                        <Folders className="mr-2 h-4 w-4" />
+                        Categories
+                     </Link>
+                  </CommandItem>
+               </CommandGroup>
+
+               <CommandSeparator />
+
+               <CommandGroup heading="Settings">
+                  <CommandItem>
+                     <User className="mr-2 h-4 w-4" />
+                     <span>Profile</span>
+                     <CommandShortcut>⌘P</CommandShortcut>
+                  </CommandItem>
+                  <CommandItem>
+                     <CreditCard className="mr-2 h-4 w-4" />
+                     <span>Billing</span>
+                     <CommandShortcut>⌘B</CommandShortcut>
+                  </CommandItem>
+                  <CommandItem>
+                     <Settings className="mr-2 h-4 w-4" />
+                     <span>Settings</span>
+                     <CommandShortcut>⌘S</CommandShortcut>
+                  </CommandItem>
+               </CommandGroup>
+            </CommandList>
+         </Command>
+      </aside>
+   );
+};
+
+export default Sidebar;
